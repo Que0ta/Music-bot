233a00360b6b7e6f50d2da892d0d0b82860d0870
que0ta <queota@supermail.com>
2025-03-21 11:30:38 +0200

    updated link

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_message():
 @app.route('/')
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://tele-check.onrender.com/' + TOKEN)  # Replace with your Render app name!
+    bot.set_webhook(url='https://music-bot-i71c.onrender.com/' + TOKEN)  # Replace with your Render app name!
     return "Webhook set!", 200
 
 
